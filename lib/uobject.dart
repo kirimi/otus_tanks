175689048566727210b1774750028f31bb2257b7
Kirill Mironov <kirill.mironov@gmail.com>
2021-12-30 16:54:21 +0300

    add mock for uobject

--- lib/uobject.dart
+++ lib/uobject.dart
@@ -10,12 +10,31 @@ class KeyNotFoundException implements Exception {
   }
 }
 
+/// Исключение при ошибке хранилища значений
+class StorageException implements Exception {
+  StorageException(this.message) : super();
+
+  final String message;
+
+  @override
+  String toString() {
+    return 'StorageException{message: $message}';
+  }
+}
+
+/// Интерфейс для UObject
+abstract class UObjectBase {
+  dynamic get(String key);
+  void set(String key, dynamic value);
+}
+
 /// Универсальный объект с динамическими свойствами
 /// если производится попытка прочитать свойство не найдено,
 /// то выкидывает [KeyNotFoundException]
-class UObject {
+class UObject implements UObjectBase {
   final Map<String, dynamic> _properties = {};
 
+  @override
   dynamic get(String key) {
     if (_properties.containsKey(key)) {
       return _properties[key];
@@ -23,6 +42,7 @@ class UObject {
     throw KeyNotFoundException('Key $key not found');
   }
 
+  @override
   void set(String key, dynamic value) {
     _properties[key] = value;
   }
